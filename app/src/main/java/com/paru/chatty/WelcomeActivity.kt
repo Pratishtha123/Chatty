a1a9ec9425a0cc68bbdbcad2f0ddab4c17fb450f
Pratishtha123 <54323039+Pratishtha123@users.noreply.github.com>
2020-06-16 02:18:55 +0530

    Welcome activity created

--- app/src/main/java/com/paru/chatty/WelcomeActivity.kt
+++ app/src/main/java/com/paru/chatty/WelcomeActivity.kt
@@ -1,12 +1,44 @@
 package com.paru.chatty
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseUser
+import kotlinx.android.synthetic.main.activity_welcome.*
 
 class WelcomeActivity : AppCompatActivity() {
+
+    var firebaseUser:FirebaseUser?=null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_welcome)
+
+        register_welcome_btn.setOnClickListener{
+            val intent=Intent(this@WelcomeActivity,RegisterActivity::class.java)
+            startActivity(intent)
+            finish()
+        }
+
+        login_welcome_btn.setOnClickListener{
+            val intent=Intent(this@WelcomeActivity,LoginActivity::class.java)
+            startActivity(intent)
+            finish()
+        }
+
+    }
+
+    override fun onStart() {
+        super.onStart()
+
+        firebaseUser=FirebaseAuth.getInstance().currentUser
+
+        if (firebaseUser!=null)
+        {
+            val intent=Intent(this@WelcomeActivity,MainActivity::class.java)
+            startActivity(intent)
+            finish()
+        }
     }
 }
