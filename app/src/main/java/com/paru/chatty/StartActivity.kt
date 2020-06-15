bb6cd8ff57d385edeb764565ba9394c39f5aa8eb
Pratishtha123 <54323039+Pratishtha123@users.noreply.github.com>
2020-06-15 17:15:37 +0530

    Connected to Firebase

---
+++ app/src/main/java/com/paru/chatty/StartActivity.kt
@@ -0,0 +1,12 @@
+package com.paru.chatty
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class StartActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_start)
+    }
+}
