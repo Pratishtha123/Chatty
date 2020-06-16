8b4dc2675a05b07e7b899d49641507f55b402133
Pratishtha123 <54323039+Pratishtha123@users.noreply.github.com>
2020-06-16 17:18:13 +0530

    Login Activity Created

--- app/src/main/java/com/paru/chatty/LoginActivity.kt
+++ app/src/main/java/com/paru/chatty/LoginActivity.kt
@@ -3,11 +3,16 @@ package com.paru.chatty
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 import androidx.appcompat.widget.Toolbar
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.database.DatabaseReference
 import kotlinx.android.synthetic.main.activity_login.*
 import kotlinx.android.synthetic.main.activity_register.*
 
 class LoginActivity : AppCompatActivity() {
+
+    private  lateinit var mAuth: FirebaseAuth
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -21,6 +26,42 @@ class LoginActivity : AppCompatActivity() {
             val intent= Intent(this@LoginActivity,WelcomeActivity::class.java)
             startActivity(intent)
             finish()
+        }
+
+        mAuth=FirebaseAuth.getInstance()
+
+        login_btn.setOnClickListener{
+            loginUser()
+        }
+    }
+
+    private fun loginUser() {
+        val email:String=email_login.text.toString()
+        val password:String=password_login.text.toString()
+
+        if(email=="")
+        {
+            Toast.makeText(this@LoginActivity,"Please enter email address", Toast.LENGTH_SHORT).show()
+        }
+        else if(password=="")
+        {
+            Toast.makeText(this@LoginActivity,"Please enter Password", Toast.LENGTH_SHORT).show()
+        }
+        else
+        {
+            mAuth.signInWithEmailAndPassword(email,password).addOnCompleteListener{task->
+                if(task.isSuccessful)
+                {
+                    val intent=Intent(this@LoginActivity,MainActivity::class.java)
+                    intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK or Intent.FLAG_ACTIVITY_NEW_TASK)
+                    startActivity(intent)
+                    finish()
+                }
+                else
+                {
+                    Toast.makeText(this@LoginActivity,"Error Message: " + task.exception!!.message.toString(),Toast.LENGTH_SHORT).show()
+                }
+            }
         }
     }
 }
