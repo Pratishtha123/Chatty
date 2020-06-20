c85355dc71c7a3dc114834283bae08e667084067
Pratishtha123 <54323039+Pratishtha123@users.noreply.github.com>
2020-06-20 20:43:53 +0530

    errors fixed

--- app/src/main/java/com/paru/chatty/Activity/MessageChatActivity.kt
+++ app/src/main/java/com/paru/chatty/Activity/MessageChatActivity.kt
@@ -52,10 +52,6 @@ class MessageChatActivity : AppCompatActivity() {
         supportActionBar!!.title=""
         supportActionBar!!.setDisplayHomeAsUpEnabled(true)
         toolbar.setNavigationOnClickListener{
-            val intent=Intent(this@MessageChatActivity,
-                WelcomeActivity::class.java)
-            intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK or Intent.FLAG_ACTIVITY_NEW_TASK)
-            startActivity(intent)
             finish()
         }
 
