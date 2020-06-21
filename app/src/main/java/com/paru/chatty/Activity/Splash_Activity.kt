1d47a73ccd167452c0cd67c80d2b8c6541745d07
Pratishtha123 <54323039+Pratishtha123@users.noreply.github.com>
2020-06-21 19:49:19 +0530

    Splash Activity added

---
+++ app/src/main/java/com/paru/chatty/Activity/Splash_Activity.kt
@@ -0,0 +1,26 @@
+package com.paru.chatty.Activity
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+import com.paru.chatty.R
+
+class Splash_Activity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+
+
+        Handler().postDelayed({
+            val intent= Intent(this@Splash_Activity, WelcomeActivity::class.java)
+            startActivity(intent)
+        },2000)
+    }
+
+    override fun onPause() {
+        super.onPause()
+        finish()
+    }
+}
