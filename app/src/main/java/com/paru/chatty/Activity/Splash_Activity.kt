49dc3e077dc8a462cb188eda731c45fc738b2568
Pratishtha123 <54323039+Pratishtha123@users.noreply.github.com>
2020-06-22 01:24:18 +0530

    Login and Register Activity Updated

--- app/src/main/java/com/paru/chatty/Activity/Splash_Activity.kt
+++ app/src/main/java/com/paru/chatty/Activity/Splash_Activity.kt
@@ -6,15 +6,15 @@ import android.os.Bundle
 import android.os.Handler
 import com.paru.chatty.R
 
-class Splash_Activity2 : AppCompatActivity() {
+class Splash_Activity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_splash2)
+        setContentView(R.layout.activity_splash)
 
 
         Handler().postDelayed({
-            val intent= Intent(this@Splash_Activity2, WelcomeActivity::class.java)
+            val intent= Intent(this@Splash_Activity, WelcomeActivity::class.java)
             startActivity(intent)
         },5000)
     }
